ea7ba5cbc6ca649578611d214a6da8553f2fe4b4
荒天帝 <1027887839@qq.com>
2018-05-22 18:37:24 +0800

    提交

---
+++ test/test/Class/CXCategory/CXNSObjectAdditions.h
@@ -0,0 +1,19 @@
+//
+//  NSObject+CXNSObjectAdditions.h
+//  test
+//
+//  Created by 曹翔 on 2018/4/8.
+//  Copyright © 2018年 CX. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <objc/runtime.h>
+
+
+NSString* kbToString(id obj);
+
+
+
+@interface NSObject (CXNSObjectAdditions)
+
+@end
---
+++ test/test/Class/CXCategory/CXNSObjectAdditions.m
@@ -0,0 +1,13 @@
+//
+//  NSObject+CXNSObjectAdditions.m
+//  test
+//
+//  Created by 曹翔 on 2018/4/8.
+//  Copyright © 2018年 CX. All rights reserved.
+//
+
+#import "CXNSObjectAdditions.h"
+
+@implementation NSObject (CXNSObjectAdditions)
+
+@end
